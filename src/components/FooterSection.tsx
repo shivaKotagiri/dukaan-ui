89e007bdb3d55633df1a9f059d603d0a0f5fa74d
shivaKotagiri <shivakotagiri532@gmail.com>
2025-03-31 18:45:16 +0530

    changed the footer section links

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -7,13 +7,14 @@ function useExternalLink(url: string) {
 }
 
 function FooterSection() {
-  const handleCode = useExternalLink("https://github.com/shivaKotagiri/");
+  const handleCode = useExternalLink("https://github.com/shivaKotagiri/dukaan-ui");
+  const handleFun=useExternalLink("https://github.com/shivaKotagiri/");
 
   return (
     <div className="justify-center flex-col w-full p-5">
       <div className="text-gray-500 text-center">
         --- Made By
-        <span onClick={handleCode} className="text-[#146fb5] font-semibold cursor-pointer"> Shiva Kumar Kotagiri</span>, inspired by
+        <span onClick={handleFun} className="text-[#146fb5] font-semibold cursor-pointer"> Shiva Kumar Kotagiri</span>, inspired by
         <span className="text-[#146fb5] font-semibold cursor-pointer"> Dukaan App UI </span>
         ---
       </div>
